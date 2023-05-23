7f03624a8947707adf473572f77f99c90fdb47b6
Vanessa Felix Christoff <vfchristoff9@gmail.com>
2023-05-23 08:12:22 -0300

    CRUD Edit fix

--- crudphp/editemp.php
+++ crudphp/editemp.php
@@ -1,7 +1,7 @@
 <?php
 //Inicializando as variáveis
 $nome = "";
-$valor = "";
+$cnpj = "";
 $status = "";
 //Inicializando os alertas
 $errorMessage = "";
@@ -26,7 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] =='GET') {
   //Pega o id atual, seleciona tudo que tem relacionado a esse id na tabela produtos (no banco de dados) 
   //E mostra na tela esses campos preenchidos
   $id = $_GET["id"];
-  $sql = "SELECT * FROM produtos WHERE id=$id";
+  $sql = "SELECT * FROM empresas WHERE id=$id";
   $result = $connection->query($sql);
   $row = $result->fetch_assoc();
 
@@ -36,7 +36,7 @@ if ($_SERVER['REQUEST_METHOD'] =='GET') {
   }
 
   $nome = $row["nome"];
-  $valor = $row["valor"];
+  $cnpj = $row["cnpj"];
   $status = $row["status"];
 
 }
@@ -46,16 +46,16 @@ else {
   //Utiliza o método post para guardar arrays de todos os inputs e o id desse formulário
   $id = $_POST["id"];
   $nome = $_POST["nome"];
-  $valor = $_POST["valor"];
+  $cnpj = $_POST["cnpj"];
   $status = $_POST["status"];
   do {
   //Se não estiverem preenchidos todos os campos do formulário, mostrar mensagem de erro
-    if ( empty($id) || empty($nome) || empty($valor) || empty($status) ) {
+    if ( empty($id) || empty($nome) || empty($cnpj) || empty($status) ) {
       $errorMessage = "É necessário preencher todos os campos do formulário.";
       break;
   }
-  //Atualiza no bando de dados as variáveis nome, valor e status de acordo com o id deste item
-  $sql ="UPDATE produtos SET nome = '$nome', valor = '$valor', status = '$status' WHERE id = $id";
+  //Atualiza no bando de dados as variáveis nome, cnpj e status de acordo com o id deste item
+  $sql ="UPDATE produtos SET nome = '$nome', cnpj = '$cnpj', status = '$status' WHERE id = $id";
   $result = $connection->query($sql);
   //Mostra o erro de conexão quando não foi possível enviar a atualização para o banco de dados
   if (!$result) {
@@ -77,13 +77,13 @@ else {
 
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" crossorigin="anonymous">            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
-  <title>Alterar Produto</title>
+  <title>Alterar Empresa</title>
 </head>
 
 <body>
 
 <div class="container my-5">
-    <h2>Alterar Produto</h2>
+    <h2>Alterar Empresa</h2>
     <?php 
     if (!empty ($errorMessage)) {
         echo "
@@ -98,23 +98,23 @@ else {
       <!-- Id secreto para não ser ajustado nem visível para o usuário, precisa existir para completar o formulário -->
       <input type ="hidden" name = "id" value="<?php  echo $id;?>">
       <div class="form-group">
-        <label for="nome">Nome do Produto</label>
+        <label for="nome">Nome da Empresa</label>
         <input type="nome" name="nome" class="form-control" value="<?php  echo $nome;?>" placeholder="Insira o nome do produto">
       </div>
       <div class="form-group">
-        <label for="valorproduto">Valor do Produto</label>
-        <input type="valor" name="valor" class="form-control" value="<?php  echo $valor;?>" placeholder="Insira o valor do produto">
+        <label for="cnpjempresa">CNPJ</label>
+        <input type="cnpj" name="cnpj" class="form-control" value="<?php  echo $cnpj;?>" placeholder="Insira o cnpj do produto">
       </div>
       <div class="form-check my-2">
-        <input class="form-check-input" type="radio" name="status" value="Sim" <?php echo ($status=='Sim')?'checked':'' ?>>
+        <input class="form-check-input" type="radio" name="status" value="Ativa" <?php echo ($status=='Ativa')?'checked':'' ?>>
         <label class="form-check-label" for="0">
-          Sim
+          Ativa
         </label>
       </div>
       <div class="form-check my-2">
-        <input class="form-check-input" type="radio" name="status" value="Não" <?php echo ($status=='Não')?'checked':'' ?>>
+        <input class="form-check-input" type="radio" name="status" value="Inativa" <?php echo ($status=='Inativa')?'checked':'' ?>>
         <label class="form-check-label" for="1">
-          Não
+          Inativa
         </label>
       </div>
 
